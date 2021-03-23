f66c43620e625e307c8e3935e35fdcefd07699dd
Mich <coolxex86@gmail.com>
2021-03-24 00:29:59 +0300

    first commit

---
+++ play.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Играть</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="/Style/playStyle.css">
+</head>
+<style>
+   body {
+     background: url("/Content/playblackground.jpg") no-repeat center top fixed;
+     -webkit-background-size: cover;
+     -moz-background-size: cover;
+     -o-background-size: cover;
+     background-size: cover;
+   }
+  </style>
+<body>
+  <div class="header">
+    <ul class="hr">
+      <li><h1 align="center" id="NameGame">e-Manager</h1></li>
+      <li id="linkAccount"><a href="#" id="styleLinkAccount">Личный кабинет</a></li>
+    </ul>
+  </div>
+  <div class="main center">
+    <ul class="hr">
+      <li><h1 align="center" id="yourteam">Твоя команда</h1></li>
+  </div>
+  <div class="left">
+    <ul class="hr">
+      <li><h1 align="left" id="stat">Статистика</h1></li>
+  </div>
+  <div class="right">
+    <ul class="hr">
+      <li><h1 align="right" id="money">Бюджет команды</h1></li>
+  </div>
+</body>
+</html>
