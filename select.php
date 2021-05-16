a216afaf6fc317ea555faaef5ffa3459fd2dcafe
Leonid <Leonid@MacBook-Air-Leonid.local>
2021-05-16 21:28:05 +0300

    Убран выбор игроков при уже запущенном ивенте

--- select.php
+++ select.php
@@ -2,6 +2,7 @@
     $selected1 = $_POST['select_card1'];$selected2 = $_POST['select_card2']; $selected3 = $_POST['select_card3'];$selected4 = $_POST['select_card4'];$selected5 = $_POST['select_card5'];
     $id_card1;$id_card2;$id_card3;$id_card4;$id_card5;
     $role_selected1;$role_selected2;$role_selected3;$role_selected4;$role_selected5;
+    $status;
     $UserName=$_COOKIE['login'];
 
     $mysql = new mysqli('localhost', 'root', 'root', 'register-bd');
@@ -14,7 +15,16 @@
     $id_card1 = join(',', $id_card1);
     $role_selected1 = join(',',$role_selected1);
 
-    $result = $mysql->query("UPDATE `users` SET idCard1 = $id_card1 WHERE login = '$UserName'");
+    $result_select = $mysql->query("SELECT * FROM `e-lig` WHERE `login`='$UserName'");
+                        while($row = mysqli_fetch_assoc($result_select))
+                                                               {
+                                                                $status[] = $row['status'];
+                                                               }
+               $status = join(',', $status);
+
+    if($status=='YES'){
+        $result = $mysql->query("UPDATE `users` SET idCard1 = $id_card1 WHERE login = '$UserName'");
+    }
 
     $result_select = $mysql->query("SELECT * FROM `list_card` WHERE `viewer_name`='$selected2'");
         while($row = mysqli_fetch_assoc($result_select))
@@ -25,7 +35,7 @@
         $id_card2 = join(',', $id_card2);
         $role_selected2 = join(',',$role_selected2);
 
-        if($id_card2 != $id_card1 and $role_selected2 != $role_selected1){
+        if($id_card2 != $id_card1 and $role_selected2 != $role_selected1 and $status=='YES'){
             $result = $mysql->query("UPDATE `users` SET idCard2 = $id_card2 WHERE login = '$UserName'");
         }
 
@@ -38,7 +48,7 @@
             $id_card3 = join(',', $id_card3);
             $role_selected3 = join(',',$role_selected3);
 
-            if($id_card3 != $id_card1 and $id_card3 != $id_card2 and $role_selected3 != $role_selected1 and $role_selected3 != $role_selected2){
+            if($id_card3 != $id_card1 and $id_card3 != $id_card2 and $role_selected3 != $role_selected1 and $role_selected3 != $role_selected2 and $status=='YES'){
                 $result = $mysql->query("UPDATE `users` SET idCard3 = $id_card3 WHERE login = '$UserName'");
             }
 
@@ -51,7 +61,7 @@
                 $id_card4 = join(',', $id_card4);
                 $role_selected4 = join(',',$role_selected4);
 
-                if($id_card4 != $id_card1 and $id_card4 != $id_card2 and $id_card4 != $id_card3 and $role_selected4 != $role_selected1 and $role_selected4 != $role_selected2 and $role_selected4 != $role_selected3){
+                if($id_card4 != $id_card1 and $id_card4 != $id_card2 and $id_card4 != $id_card3 and $role_selected4 != $role_selected1 and $role_selected4 != $role_selected2 and $role_selected4 != $role_selected3 and $status=='YES'){
                     $result = $mysql->query("UPDATE `users` SET idCard4 = $id_card4 WHERE login = '$UserName'");
                 }
 
@@ -64,7 +74,7 @@
                     $id_card5 = join(',', $id_card5);
                     $role_selected5 = join(',',$role_selected5);
 
-                    if($id_card5 != $id_card1 and $id_card5 != $id_card2 and $id_card5 != $id_card3 and $id_card5 != $id_card4 and $role_selected5 != $role_selected1 and $role_selected5 != $role_selected2 and $role_selected5 != $role_selected3 and $role_selected5 != $role_selected4){
+                    if($id_card5 != $id_card1 and $id_card5 != $id_card2 and $id_card5 != $id_card3 and $id_card5 != $id_card4 and $role_selected5 != $role_selected1 and $role_selected5 != $role_selected2 and $role_selected5 != $role_selected3 and $role_selected5 != $role_selected4 and $status=='YES'){
                         $result = $mysql->query("UPDATE `users` SET idCard5 = $id_card5 WHERE login = '$UserName'");
                     }
 
